e9f84cbf34ef223b97a570c3819d97c26ad02ec5
gary corcorann <gary.corcoran@mail.mcgill.ca>
2017-11-24 11:48:50 -0500

    Created and tested HoG class.

--- hog.py
+++ hog.py
@@ -1,22 +1,58 @@
 import cv2
 
+class HOG():
+    """ Histogram of Orientated Gradients Model. """
+
+    def __init__(self, img_path, winSize, blockSize, blockStride, 
+            cellSize, nbins):
+        """
+        Initialize parameters.
+        
+        @param  img_path:       path to input image
+        @param  winSize:        HoG window size
+        @param  blockSize:      HoG block size
+        @param  blockStride:    HoG block stride
+        @param  cellSize:       HoG cell size
+        @param  nbins:          HoG number of bins
+        """
+        self.img_path = img_path
+        self.__winSize = winSize
+        self.hog = cv2.HOGDescriptor(winSize, blockSize, blockStride,
+                cellSize, nbins)
+
+    def __preprocess(self):
+        """
+        Perform preprocessing on image.
+
+        @return img:    input image after preprocessing
+        """
+        img = cv2.imread(self.img_path)
+        # resize input image to same shape as hog window
+        img = cv2.resize(img, self.__winSize)
+        return img
+
+    def compute(self):
+        """
+        Compute HoG features.
+        
+        @return h:  array of HoG features [num_feats x 1]
+        """
+        img = self.__preprocess()
+        h = self.hog.compute(img)
+        return h
+
 def main():
     """ Main Function. """
     img_path = 'lena.jpg'
-    img = cv2.imread(img_path)
-    img = cv2.resize(img, (208, 304))
-    
-    # HOG parameters
-    winSize = (208, 304)
-    blockSize = (32, 32)
-    blockStride = (16, 16)
-    cellSize = (16, 16)
-    nbins = 9
 
-    # compute HOG descriptor
-    hog = cv2.HOGDescriptor(winSize, blockSize, blockStride,
-            cellSize, nbins)
-    h = hog.compute(img)
+    # HOG parameters
+    hog_params = {'winSize': (208, 304), 'blockSize': (32, 32),
+            'blockStride': (16, 16), 'cellSize': (16, 16),
+            'nbins': 9}
+
+    # create HoG object
+    hog = HOG(img_path, **hog_params)
+    h = hog.compute()
     print(h.shape)
 
 if __name__ == '__main__':

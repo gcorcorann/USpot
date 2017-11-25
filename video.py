1b32af5d526f6511e6c56b865ae6800e920a430c
Gary Corcoran <gary.corcoran@mail.mcgill.ca>
2017-11-24 22:19:57 -0500

    Updated video player algorithm, added basic video processing.

--- video.py
+++ video.py
@@ -5,6 +5,7 @@ Video Player Module.
 @date: Nov. 24th, 2017
 """
 
+import numpy as np
 import cv2
 
 class Video():
@@ -16,7 +17,7 @@ class Video():
         @param  video_path: path to input video file
         """
         self.video_path = video_path
-        self.__cap = None
+        self._cap = None
 
     def set_video_path(self, video_path):
         """
@@ -24,7 +25,7 @@ class Video():
 
         @param  video_path: path to input video file
 
-        @modifies   self.video_path: stores video file
+        @modifies   self.video_path:    stores video file
         """
         self.video_path = video_path
 
@@ -36,32 +37,108 @@ class Video():
         """
         return self.video_path
 
+    def _is_opened(self):
+        """
+        Check if video is opened.
+
+        @return ret:    true if video is opened, else false
+        """
+        ret = self._cap.isOpened()
+        if ret is False:
+            print('Could not opened video file stored at:', self.video_path)
+        return ret
+
+    def _read(self):
+        """
+        Read video frame.
+
+        @return ret:    false end of file, else true
+        @return frame:  video frame
+        """
+        ret, frame = self._cap.read()
+        if ret is False:
+            print('Reached end of file.')
+        return ret, frame
+
+    def _release(self):
+        """
+        Release resources.
+        """
+        self._cap.release()
+        cv2.destroyAllWindows()
+
+    def _display(self, frame):
+        """
+        Display video frame.
+        
+        @return ret:    false if user quit video, else true
+        """
+        cv2.imshow('Frame', frame)
+        key = cv2.waitKey(1)
+        # if user press 'q'
+        if key == 113:
+            print('User quit video.')
+            return False
+        return True
+
+    def _process(self, frame):
+        """
+        Process video frame.
+
+        @param  frame:  input video frame
+
+        @return frame:  processed video frame
+        """
+        # resize frame
+        frame = cv2.resize(frame, None, fx=0.5, fy=0.5) 
+        # get new image dimensions
+        rows, cols = frame.shape[:2]
+        # rotate frame
+        M = cv2.getRotationMatrix2D((cols/2, rows/2), -90, 1)
+        # bottem left pixel
+        bl = [0, rows, 1]
+        # find new position
+        bl = np.int_(np.dot(M, bl))
+        frame = cv2.warpAffine(frame, M, (cols, rows))
+        # crop frame (i.e. remove black columns) using transformed 
+        # pixel location
+        frame = frame[:, bl[0]:bl[1]]
+        return frame
+
     def run(self):
         """
         Play video stored in video_path.
         """
+        print("Press 'q' to quit video.")
         if self.video_path is None:
             print('Please input video path before running')
             return
-        self.__cap = cv2.VideoCapture(0)
-        print(self.__cap.isOpened())
-        while self.__cap.isOpened():
+        self._cap = cv2.VideoCapture(self.video_path)
+        while self._is_opened():
             # read frame
-            ret, frame = self.__cap.read()
-            print(ret)
-            # display
-            cv2.imshow('Frame', frame)
-            if cv2.waitKey(0) & 0xFF == ord('q'):
+            ret, frame = self._cap.read()
+            if ret is False:
                 break
-        self.__cap.release()
-        cv2.destroyAllWindows()
-        
+            #TODO PROCESS VIDEO FRAME
+            frame = self._process(frame)
+            # display
+            if self._display(frame) is False:
+                break
+        # release resources
+        self._release() 
 
 def main():
     """ Main Function. """
-    video_path = 'dataset/IMG_0687.MOV'
-    video_path = '/home/gary/opencv-3.2.0/samples/data/vtest.avi'
+    import sys
 
+    # check command line inputs
+    if len(sys.argv) >= 2:
+        print(sys.argv[1])
+        # set command line input to video path
+        video_path = sys.argv[1]
+    else:
+        # set default video path
+        video_path = 'dataset/IMG_0687.MOV'
     # create video player object
     vod = Video(video_path)
     vod.run()
